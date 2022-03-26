3abec74164071e1909bbb467405ca04a75adc067
adnarimanad <3262miranda@gmail.com>
2022-03-26 17:57:53 +0800

    dana -> update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,39 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="{{ asset('Manage/css/style.css')}}" type="text/css">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-<body>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+
+   
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ asset('Manage/css/style.css')}}" type="text/css">
+
+  </head>
+  
+  <body>
+  
+
+  
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="{{ asset('Manage/img/login.png') }}" alt="Image" class="img-fluid">
+        </div>
+        <div class="col-md-6 contents">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="mb-4">
                 <div class="text-center py-3"><h1 class="display-5">{{ __('SIGN IN') }}</h1></div>
+            </div>
+            <form method="POST" action="{{ route('login') }}">
+              @csrf
+              <div class="form-group first">
+                <input id="email" type="email" class="form-control  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email" required autocomplete="email" autofocus>
+                @error('email')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+                @enderror
 
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row mr-1">
-                            <label for="email" class="col-5 col-form-label ">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-7 p-0 m-0">
-                                <input id="email" type="email" class="form-control  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mr-1">
-                            <label for="password" class="col-5 col-form-label">{{ __('Password') }}</label>
+              </div>
+              <div class="form-group last mb-4">
+                <label for="password" class="col-5 col-form-label">{{ __('Password') }}</label>
 
                             <div class="col-7 p-0 m-0">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -41,21 +54,18 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-5 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                
+              </div>
+              
+              <div class="d-flex mb-5 align-items-center" style="margin-left: 20px;">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
-                                </div>
-                            </div>
-                        </div>
+              </div>
 
-                        <div class="form-group row mb-0">
+              <div class="form-group row mb-0">
                             <div class="col-12 d-flex justify-content-center">
                                 <button type="submit" class="btn btn-primary btn-block mx-3 border-0">
                                     {{ __('Login') }}
@@ -67,12 +77,22 @@
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                    </form>
-                </div>
+              </div>
+              
+              
             </div>
+          </div>
+          
         </div>
+        
+      </div>
     </div>
-</div>
-</body>
+  </div>
+
+  
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
 @endsection
