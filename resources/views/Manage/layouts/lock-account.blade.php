fe513947a3c5f6682abbb88b378eab601f45f62d
SenpaiXYZ <jhicer@gmail.com>
2022-04-03 21:10:01 +0800

    pulling new update

---
+++ resources/views/Manage/layouts/lock-account.blade.php
@@ -0,0 +1,13 @@
+@extends('Manage.layouts.app')
+
+@section('content')
+    <div class="main-content " id="panel">
+    @include('Manage.includes.header')
+SDFSDF
+    </div>
+@endsection
+@push('scripts')
+<script>
+
+</script>
+@endpush
